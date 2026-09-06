f00a1f3638193c8293a1e322555e9b3cd53bb889
Florian Reuth <git@florianreuth.de>
2026-09-06 17:35:16 +0200

    Also filter blank icons in BCServerInfo
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- src/main/java/de/florianreuth/classic4j/model/betacraft/BCServerInfo.java
+++ src/main/java/de/florianreuth/classic4j/model/betacraft/BCServerInfo.java
@@ -42,7 +42,7 @@ public record BCServerInfo(String name,
 ) {
 
     public Optional<byte[]> icon() {
-        if (this._icon == null) {
+        if (this._icon == null || this._icon.isBlank()) {
             return Optional.empty();
         }
 
